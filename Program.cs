306cfce9793b88f0efed9163311ac383c1630a06
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 20:03:59 -0700

    Peer review by: Kenneth Fujimura

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿// Andrea Burr
 // 10-18-2022
 // Mini4 redo - github
+//Peer Review By: Kenneth Fujimura
+//Project works. Code is easy to follow. A few points of constructive criticism:
+//- There's no real data validation that I can tell. If you wanted to do a data validation on the number inputs to make sure only numbers are inputted, a TryParse like we used in the Codewars would work for that purpose.
+//- If I recall the instructions for this project, there are pretty explicit instructions regarding making comparisons between the two inputted numbers. If you want to refactor your program, make sure you're reading the instructions carefully so your program fulfills all of the scenarios that are outlined. For example, your first if else statement only checks to see if one number is bigger than the other, and doesn't account for any scenarios where both numbers are equal.
+//- Make sure you're also naming the file folder on your computer, as well as the repository in GitHub with the same naming conventions as outlined in the LMS instructions. That includes adding a program description in your vs code comments, and in the README. And also make sure that the peer review exists both in your README files, as well as in the comments at the top of the vs code in your program.
 Console.Clear ();
 bool playagain = true;
 
